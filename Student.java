fb112ccd8ca25fba4b09cc566e31159de72e1bf9
vipul raj <122695621+vipul-0711@users.noreply.github.com>
2023-04-11 02:08:57 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,19 @@
+class Student {
+    int roll;
+    String name;
+    int sec;
+
+    class test {
+        public void main(String[] args) {
+            Student s1 = new Student();
+            //display details of student
+            s1.roll = 44;
+            s1.name = "Vipul";
+            s1.sec = 004;
+
+
+            System.out.println(s1.roll + " " + s1.name + " " + s1.sec);
+
+        }
+    }
+}
